dd817cd6262703e0f534eee255b14b869c12ef5d
Phan Tiến Đạt <104376586+tiendat2k6@users.noreply.github.com>
2026-07-17 13:10:58 +0700

    Add models for Course, Lesson, Question, and Submission

---
+++ models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.conf import settings
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    grade = models.IntegerField(default=1)
+
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        return False
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+class Submission(models.Model):
+    enrollment = models.IntegerField()
+    choices = models.ManyToManyField(Choice)
