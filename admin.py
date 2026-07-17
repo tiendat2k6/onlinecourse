466aa7614b4bdea3d93abd24aaeea3a275199b1f
Phan Tiến Đạt <104376586+tiendat2k6@users.noreply.github.com>
2026-07-17 13:55:42 +0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Course, Lesson, Question, Choice, Submission
-from django.contrib.auth.models import User, Group
+from .models import Course, Lesson, Question, Choice, Submission, Enrollment
 
 class ChoiceInline(admin.StackedInline):
     model = Choice
@@ -10,15 +9,26 @@ class QuestionInline(admin.StackedInline):
     model = Question
     extra = 2
 
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 2
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ['name', 'description']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course']
+    list_filter = ['course']
+    search_fields = ['title', 'description']
+
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ['question_text', 'grade']
 
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
-
-admin.site.register(Course)
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
+admin.site.register(Enrollment)
